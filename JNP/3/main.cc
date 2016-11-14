a677c3519192084ab2f1f4d831eccb7eb9cb061a
Vlad <vlad7mail@gmail.com>
2016-11-14 11:56:23 +0100

    First commit for the 3rd lab. Implemented basic point functionality

---
+++ JNP/3/main.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#include "point.h"
+
+#define F first
+#define S second
+#define pb push_back
+#define INF (1 << 30)
+#define SQR(a) ((a) * (a))
+
+using namespace std;
+
+const int N = 1111;
+
+int main()
+{
+    Point p(10, 10);
+    cout << p.x() << endl;    
+    return 0;
+}
