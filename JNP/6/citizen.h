800b7b2432feceb5d567a7a7d2f895c4e9efecbb
R-god <rostislavmat@gmail.com>
2017-01-11 21:50:33 +0100

    Update citizen.h

--- JNP/6/citizen.h
+++ JNP/6/citizen.h
@@ -2,37 +2,37 @@
 #define CITIZEN_H
 
 #include "helper.h"
-
+#include <memory>
 class Citizen : public _LivingEntity, public _AgeingEntity {
-  public:
-    Citizen(HealthPoints health, Age age);
+public:
+	Citizen(HealthPoints health, Age age);
 
-  private:
+private:
 };
 
 class Adult : public Citizen {
-  public:
-    Adult(HealthPoints health, Age age);
+public:
+	Adult(HealthPoints health, Age age);
 
-  private:
-    bool checkAge(Age age) override final;
+private:
+	bool checkAge(Age age) override final;
 };
 
 class Teenager : public Citizen {
-  public:
-    Teenager(HealthPoints health, Age age);
-    bool checkAge(Age age) final;
+public:
+	Teenager(HealthPoints health, Age age);
+	bool checkAge(Age age) final;
 };
 
 class Sheriff : public Adult, public _AttackingEntity {
-  public:
-    Sheriff(HealthPoints health, Age age, AttackPower attackPower);
+public:
+	Sheriff(HealthPoints health, Age age, AttackPower attackPower);
 };
 
-Adult *createAdult(HealthPoints health, Age age);
+std::shared_ptr<Adult> createAdult(HealthPoints health, Age age);
 
-Teenager *createTeenager(HealthPoints health, Age age);
+std::shared_ptr<Teenager> createTeenager(HealthPoints health, Age age);
 
-Sheriff *createSheriff(HealthPoints health, Age age, AttackPower attackPower);
+std::shared_ptr<Sheriff> createSheriff(HealthPoints health, Age age, AttackPower attackPower);
 
 #endif // CITIZEN_H
