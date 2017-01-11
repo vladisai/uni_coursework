09210c151406680cff0d8b43d2b51478ff2b0603
R-god <rostislavmat@gmail.com>
2017-01-11 21:49:20 +0100

    Update monster.h

--- JNP/6/monster.h
+++ JNP/6/monster.h
@@ -4,6 +4,7 @@
 #include "helper.h"
 #include <initializer_list>
 #include <set>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -12,65 +13,66 @@ using std::initializer_list;
 using std::string;
 using std::set;
 
+
 class Monster : public virtual _EntityWithHealth, public virtual _EntityWithAttackPower {
-  public:
-    virtual string getName() = 0;
+public:
+	virtual string getName() = 0;
 };
 
 class SingleMonster : public virtual Monster, public virtual _LivingEntity, public virtual _AttackingEntity {
-  public:
-    SingleMonster(HealthPoints health, AttackPower attackPower);
+public:
+	SingleMonster(HealthPoints health, AttackPower attackPower);
 };
 
 class Zombie : public virtual SingleMonster {
-  public:
-    Zombie(HealthPoints health, AttackPower attackPower);
-    string getName() override;
+public:
+	Zombie(HealthPoints health, AttackPower attackPower);
+	string getName() override;
 
-  private:
-    static const string CreatureName;
+private:
+	static const string CreatureName;
 };
 
 class Vampire : public virtual SingleMonster {
-  public:
-    Vampire(HealthPoints health, AttackPower attackPower);
-    string getName() override;
+public:
+	Vampire(HealthPoints health, AttackPower attackPower);
+	string getName() override;
 
-  private:
-    static const string CreatureName;
+private:
+	static const string CreatureName;
 };
 
 class Mummy : public virtual SingleMonster {
-  public:
-    Mummy(HealthPoints health, AttackPower attackPower);
-    string getName() override;
+public:
+	Mummy(HealthPoints health, AttackPower attackPower);
+	string getName() override;
 
-  private:
-    static const string CreatureName;
+private:
+	static const string CreatureName;
 };
 
-Zombie *createZombie(HealthPoints health, AttackPower attackPower);
+std::shared_ptr<Zombie> createZombie(HealthPoints health, AttackPower attackPower);
 
-Vampire *createVampire(HealthPoints health, AttackPower attackPower);
+std::shared_ptr<Vampire> createVampire(HealthPoints health, AttackPower attackPower);
 
-Mummy *createMummy(HealthPoints health, AttackPower attackPower);
+std::shared_ptr<Mummy> createMummy(HealthPoints health, AttackPower attackPower);
 
 class GroupOfMonsters : public Monster {
-  public:
-    GroupOfMonsters(const vector<SingleMonster *> monsters);
+public:
+	GroupOfMonsters(const vector<std::shared_ptr<SingleMonster> > monsters);
 
-    GroupOfMonsters(const initializer_list<SingleMonster *> monsters);
+	GroupOfMonsters(const initializer_list<std::shared_ptr<SingleMonster> > monsters);
 
-    string getName() override;
+	string getName() override;
 
-    virtual void takeDamage(AttackPower attackPower) override;
+	virtual void takeDamage(AttackPower attackPower) override;
 
-    virtual AttackPower getAttackPower() override;
+	virtual AttackPower getAttackPower() override;
 
-    HealthPoints getHealth() override;
+	HealthPoints getHealth() override;
 
-  private:
-    const set<SingleMonster *> monsters;
+private:
+	const set<std::shared_ptr<SingleMonster> > monsters;
 };
 
 #endif // MONSTER_H
