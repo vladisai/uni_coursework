2e85c78d5d5a3bcd4f217f926ca4d4f3cf8586fc
Vlad <vlad7mail@gmail.com>
2017-01-12 18:28:07 +0100

    Revert "Update monster.cc"
    This reverts commit d9418e638ea2b740593ea7584ddb39783967e273.

--- JNP/6/citizen.cc
+++ JNP/6/citizen.cc
@@ -2,39 +2,39 @@
 #include <cassert>
 
 Citizen::Citizen(HealthPoints health, Age age)
-	: _LivingEntity(health), _AgeingEntity(age) {}
+    : _LivingEntity(health), _AgeingEntity(age) {}
 
 Adult::Adult(HealthPoints health, Age age) : Citizen(health, age) {
-	checkAge(age);
+    checkAge(age);
 }
 
 bool Adult::checkAge(Age age) {
-	if (age < 18 || age > 100)
-		return false;
-	return true;
+    if (age < 18 || age > 100)
+        return false;
+    return true;
 }
 
 Teenager::Teenager(HealthPoints health, Age age) : Citizen(health, age) {
-	checkAge(age);
+    checkAge(age);
 }
 
 bool Teenager::checkAge(Age age) {
-	if (age < 11 || age > 18)
-		return false;
-	return true;
+    if (age < 11 || age > 18)
+        return false;
+    return true;
 }
 
 Sheriff::Sheriff(HealthPoints health, Age age, AttackPower attackPower)
-	: Adult(health, age), _AttackingEntity(attackPower) {}
+    : Adult(health, age), _AttackingEntity(attackPower) {}
 
-std::shared_ptr<Adult> createAdult(HealthPoints health, Age age) {
-	return  std::shared_ptr<Adult>( new Adult(health, age) );
+Adult *createAdult(HealthPoints health, Age age) {
+    return new Adult(health, age);
 }
 
-std::shared_ptr<Teenager> createTeenager(HealthPoints health, Age age) {
-	return std::shared_ptr<Teenager> ( new Teenager(health, age) );
+Teenager *createTeenager(HealthPoints health, Age age) {
+    return new Teenager(health, age);
 }
 
-std::shared_ptr<Sheriff> createSheriff(HealthPoints health, Age age, AttackPower attackPower) {
-	return std::shared_ptr<Sheriff>(new Sheriff(health, age, attackPower) ) ;
+Sheriff *createSheriff(HealthPoints health, Age age, AttackPower attackPower) {
+    return new Sheriff(health, age, attackPower);
 }
